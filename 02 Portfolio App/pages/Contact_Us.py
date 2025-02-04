34ea999540d8b89ef1baaa0af5be5b569959030c
Manuel Altermatt <manuel.altermatt@outlook.com>
2025-02-04 12:24:49 +0100

    Create an email body with the top news and send it

--- 02 Portfolio App/pages/Contact_Us.py
+++ 02 Portfolio App/pages/Contact_Us.py
@@ -7,12 +7,16 @@ with st.form(key="email_forms"):
     user_email = st.text_input("Your email")
     topic = st.selectbox("What topic do you want to discuss?", ("General Inquiry", "Support", "Feedback", "Other"))
     raw_message = st.text_area("Your message")
+    
+    # Format the email message
     message = f"""\
 Subject: New email from {user_email}
 From: {user_email}
 Topic: {topic}
 {raw_message}
     """
+    
+    
     button = st.form_submit_button("Send")
     if button:
         send_email(message)
--- 05 API App/main.py
+++ 05 API App/main.py
@@ -1,6 +1,27 @@
 import requests
+import smtplib, ssl
+import io
+import sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
 
 # 70eaa7ba48814f049c265626844bf5cb
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "manuel.altermatt@gmail.com"
+    password = "qgpd okcn kbum ptlr"
+
+    receiver = "manuel.altermatt@gmail.com"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 
 url = "https://newsapi.org/v2/everything?q=python&sortBy=popularity&apiKey=70eaa7ba48814f049c265626844bf5cb"
@@ -16,7 +37,26 @@ request = requests.get(url)
 # Get the content of the response
 content = request.json()
 
-# Access the articles
-articles = content["articles"]
+# Access the top 10 articles
+articles = content["articles"][:10]
+
+
+# Create a message made of the top articles
+message = """Top Headlines\n\n"""
+
 for article in articles:
-    print(article["title"])
+    message += f"{article['title']}\n"
+    # print(article["publishedAt"])
+
+
+# Send the email
+send_email(message)
+
+# article
+# source
+# author
+# title
+# description
+# url
+# content
+# Set the encoding to utf-8
